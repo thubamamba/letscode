bd0577c15413fadac0b288df3eeac4f5f60ebd41
Thubz <tsmmamba@gmail.com>
2019-06-27 11:22:54 +0200

    removed tags around DB credentials

--- config.js
+++ config.js
@@ -12,6 +12,6 @@ module.exports = {
     //dbConnstring: 'mongodb://127.0.0.1:27017/letscode',
 
     //mLab DB - AWS
-    dbConnstring: 'mongodb://<thubz>:<Lungithuba94>@ds243317.mlab.com:43317/letscode',
+    dbConnstring: 'mongodb://thubz:Lungithuba94@ds243317.mlab.com:43317/letscode',
     sessionKey: 'WolaLetsCode'
 }
